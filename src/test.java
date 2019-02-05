f58f6a813710e7e7412c88bdd318cc415e477c3a
parandbazollahi <47065252+parandbazollahi@users.noreply.github.com>
2019-02-04 20:21:37 -0600

    update
    update

--- src/test.java
+++ src/test.java
@@ -3,6 +3,6 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stubsysrem
-		System.out.print("hello word");
+		System.out.print("hello word 2");
 	}
 }
